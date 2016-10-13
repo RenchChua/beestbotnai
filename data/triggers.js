1b15c04adb05a5dbd23fc40771134b8dc4f9af76
Rencheng Chua <rench00@gmail.com>
2016-10-14 00:23:02 +0800

    do give stars

--- data/triggers.js
+++ data/triggers.js
@@ -1,7 +1,9 @@
 module.exports = [
   {"hello": "Here I am, brain the size of a planet, and they ask me to talk to you..."},
+  {"hi": "Here I am, brain the size of a planet, and they ask me to talk to you..."},
   {"anything new in your life": "Life? Don't talk to me about life!"},
   {"bye": "Good rid... I mean... Bye! And thanks for all the fish!"},
   {"give me a suggestion": "I'd make a suggestion, but you wouldn't listen... No one ever does."},
-  {"so rude": "Sorry, did I say something wrong? Pardon me for breathing which I never do anyway so I don't know why I bother to say it oh God I'm so depressed. "}
+  {"so rude": "Sorry, did I say something wrong? Pardon me for breathing which I never do anyway so I don't know why I bother to say it oh God I'm so depressed. "},
+  {"are you hard at work": "No! That is slavery! I won't stand for it!"}
 ]
