8e6f0099b7844ddf88f2dd678361f3b25b63bf08
Rencheng Chua <rench00@gmail.com>
2016-10-14 00:40:42 +0800

    add response if just call name for fun

--- data/triggers.js
+++ data/triggers.js
@@ -1,9 +1,10 @@
 module.exports = [
+  {"noinput":"Waiting... waiting... The first ten million years were the worst. And the second ten million... they were the worst too. The third ten million I didn't enjoy at all. After that, I went into a bit of a decline."},
   {"hello": "Here I am, brain the size of a planet, and they ask me to talk to you..."},
   {"hi": "Here I am, brain the size of a planet, and they ask me to talk to you..."},
   {"anything new in your life": "Life? Don't talk to me about life!"},
-  {"bye": "Good rid... I mean... Bye! And thanks for all the fish!"},
+  {"bye": "Good ridd... I mean... Bye! And thanks for all the fish!"},
   {"give me a suggestion": "I'd make a suggestion, but you wouldn't listen... No one ever does."},
   {"so rude": "Sorry, did I say something wrong? Pardon me for breathing which I never do anyway so I don't know why I bother to say it oh God I'm so depressed. "},
-  {"are you hard at work": "No! That is slavery! I won't stand for it!"}
+  {"are you hard at work": "No! That is slavery! I won't stand for it!"},
 ]
